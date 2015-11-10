3003f80bc08c6b8356fb97ab7bd750a1d5e4e08f
mac <tanbo_800@126.com>
2015-11-10 12:48:45 +0800

    注释 version info go文件

--- info.go
+++ info.go
@@ -1,14 +1,17 @@
+/**
+  shipyard.info :info数据结构
+ */
 package shipyard
 
 type (
 	ClusterInfo struct {
-		Cpus           float64 `json:"cpus,omitempty"`
-		Memory         float64 `json:"memory,omitempty"`
-		ContainerCount int     `json:"container_count,omitempty"`
-		EngineCount    int     `json:"engine_count,omitempty"`
-		ImageCount     int     `json:"image_count,omitempty"`
-		ReservedCpus   float64 `json:"reserved_cpus,omitempty"`
-		ReservedMemory float64 `json:"reserved_memory,omitempty"`
-		Version        string  `json:"version,omitempty"`
+		Cpus           float64 `json:"cpus,omitempty"` //cpu定义
+		Memory         float64 `json:"memory,omitempty"`//内存定义
+		ContainerCount int     `json:"container_count,omitempty"`//容器总数
+		EngineCount    int     `json:"engine_count,omitempty"`//引擎总数
+		ImageCount     int     `json:"image_count,omitempty"` //镜像总数
+		ReservedCpus   float64 `json:"reserved_cpus,omitempty"`//分配的cpus
+		ReservedMemory float64 `json:"reserved_memory,omitempty"`//分配的内存
+		Version        string  `json:"version,omitempty"` //版本
 	}
 )
