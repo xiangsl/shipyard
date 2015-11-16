b37ef2a63be1227fdd9cde57ec8132cfde5788df
mac <tanbo_800@126.com>
2015-11-16 21:41:21 +0800

    错误代码定义

--- registry/v1/error.go
+++ registry/v1/error.go
@@ -4,12 +4,18 @@ import (
 	"fmt"
 )
 
+/*
+	错误信息类型
+ */
 type Error struct {
-	StatusCode int
-	Status     string
-	msg        string
+	StatusCode int  //状态代码
+	Status     string //状态
+	msg        string //消息内容
 }
 
+ /**
+  打印错误信息
+  */
 func (e Error) Error() string {
 	return fmt.Sprintf("%s: %s", e.Status, e.msg)
 }
